ec5cfb29014539fa8ea5c04364541b93f0dc1a9d
wookie <kononenkoa995@ukr.net>
2016-03-26 14:39:36 +0200

    first commit

---
+++ Functions.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Functions.cpp
+ * Author: wookie
+ * 
+ * Created on March 25, 2016, 8:02 PM
+ */
+
+#include "Functions.h"
+
+Functions::Functions() {
+}
+
+Functions::Functions(const Functions& orig) {
+}
+
+Functions::~Functions() {
+}
+
+double Functions::myfunc(double x1, double x2) {
+    return x1 + x2;
+}
+
