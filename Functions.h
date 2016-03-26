a866b2764f26d731afba4ae85b245792ea75d471
wookie <kononenkoa995@ukr.net>
2016-03-26 14:39:31 +0200

    first commit

---
+++ Functions.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Functions.h
+ * Author: wookie
+ *
+ * Created on March 25, 2016, 8:02 PM
+ */
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+class Functions {
+public:
+    Functions();
+    Functions(const Functions& orig);
+    virtual ~Functions();
+    
+    double myfunc(double x1, double x2);
+private:
+
+};
+
+#endif /* FUNCTIONS_H */
+
