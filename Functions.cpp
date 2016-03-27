e838a85f7ad1b2f0c7187f32e62c79a61719aa18
wookie <kononenkoa995@ukr.net>
2016-03-27 21:13:01 +0300

    add some new functions

--- Functions.cpp
+++ Functions.cpp
@@ -41,3 +41,40 @@ double Functions::hyper_sphere(std::vector<double> x) {
     }
     return sum;
 }
+
+double Functions::rosenbrock1(std::vector<double> x) {
+    return 100*pow((x[0]*x[0] - x[1]), 2) + pow((1 - x[0]), 2);
+}
+
+double Functions::rosenbrock2(std::vector<double> x) {
+    double sum = 0;
+    
+    for(int i = 0; i < 3; i)
+        sum += 100*pow((x[i]*x[i] - x[i+1]), 2) + pow((1 - x[i]), 2);
+    
+    return sum; 
+}
+
+double Functions::griewangk(std::vector<double> x) {
+    double sum = 0;
+    double mult = 1;
+    
+    for(int i = 0; i < 10; i++) {
+        sum += x[i]*x[i]/4000;
+        mult *= cos(x[i]/sqrt(i+1));
+    }
+    
+    return 1/(0.1 + sum - mult + 1);
+}
+
+//double Functions::griewangk(std::vector<double> x) {
+//    double sum = 0;
+//    double mult = 1;
+//    
+//    for(int i = 0; i < 10; i++) {
+//        sum += x[i]*x[i]/4000;
+//        mult *= cos(x[i]/sqrt(i+1));
+//    }
+//    
+//    return mult;
+//}
